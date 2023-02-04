ea1c812772a5d25824b2dff3555c7213d2a71c4f
MuhammadHastin <hastinre@gmail.com>
2023-02-04 19:49:26 +0700

    push 8

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,7 @@ function Header() {
     e.preventDefault()
     document.body.classList.toggle('sb-sidenav-toggled');
     localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+    console.log(localStorage.getItem('sb|sidebar-toggle'))
   }
   return (
     <>
