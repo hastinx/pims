eac89b9cde568963dc8d6ee8af1980297aff76fc
MuhammadHastin <hastinre@gmail.com>
2023-02-27 10:30:59 +0700

    push60

--- src/pages/content/overview/Blocksummary.js
+++ src/pages/content/overview/Blocksummary.js
@@ -1,11 +1,51 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { Button, Modal } from 'react-bootstrap';
+import Swal from 'sweetalert2';
+import { useGetApi } from '../../../server/Api';
 
 function Blocksummary() {
+    const [show, setShow] = useState(false);
+    const [bopd, setBopd] = useState("");
+    const [mmscfd, setMmscfd] = useState("");
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    const handleUpdate = async () => {
+        await axios.post(
+            process.env.REACT_APP_API_URL + "user",
+            {
+                userid: bopd,
+                password: mmscfd,
+            }
+        );
+    }
+
+    const GetData = async () => {
+        const data = await useGetApi("pad_a/wellhead/m1");
+        if (data.error === false) {
+            setBopd(data.data.value.bpod)
+            setMmscfd(data.data.value.mmscfd)
+        } else {
+            Swal.fire({
+                title: "Oops!",
+                text: "Block Summary " + data.message,
+                icon: "error",
+                confirmButtonText: "OK",
+            });
+        }
+    };
+
+    useEffect(() => {
+        GetData()
+    }, [bopd])
+
     return (
         <>
             <div className="card border-secondary">
-                <div className="card-header fw-semibold text-center text-white bg-secondary">
-                    BLOCK SUMMARY
+                <div className="card-header fw-semibold text-center text-white bg-secondary d-flex justify-content-between">
+                    BLOCK SUMMARY <button className='btn btn-sm btn-primary left' onClick={handleShow}><i className='fa fa-pencil fs-6' /></button>
                 </div>
                 <div className="card-body">
                     <div className='row'>
@@ -40,6 +80,35 @@ function Blocksummary() {
                     </div>
                 </div>
             </div>
+
+            <Modal
+                show={show}
+                onHide={handleClose}
+                backdrop="static"
+                keyboard={false}
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title>Edit Block Summary</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <form className='p-2'>
+                        <div className='form-group row align-items-center mb-2 g-2'>
+                            <label className='form-label col-2'>BOPD</label>
+                            <input className='form-control col' type='text' name='bopd' onChange={(e) => setBopd(e.target.value)} placeholder='BOPD' />
+                        </div>
+                        <div className='row align-items-center g-2'>
+                            <label className='form-label col-2'>MMSCFD</label>
+                            <input className='form-control col' type='text' name='mmscfd' onChange={(e) => setMmscfd(e.target.value)} placeholder='MMSCFD' />
+                        </div>
+                    </form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={() => handleUpdate()}>Update</Button>
+                </Modal.Footer>
+            </Modal>
         </>
     )
 }
